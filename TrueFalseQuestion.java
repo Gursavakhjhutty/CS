6a534d932d8a82f8f0d31659bce40854a246017b
Gursavakhjhutty <117145051+Gursavakhjhutty@users.noreply.github.com>
2023-05-09 22:06:31 -0600

    Update TrueFalseQuestion.java

--- TrueFalseQuestion.java
+++ TrueFalseQuestion.java
@@ -1,19 +1,35 @@
+import javax.swing.JOptionPane;
+
 public class TrueFalseQuestion extends Question {
     public TrueFalseQuestion(String question, String answer) {
         this.question = question;
-        this.correctAnswer = answer.toUpperCase(Locale.getDefault());
+        this.correctAnswer = answer.toUpperCase();
     }
 
+    @Override
     public String ask() {
         while (true) {
             String answer = JOptionPane.showInputDialog(question + " (True/False)");
-            answer = answer.toUpperCase(Locale.getDefault());
+            answer = answer.toUpperCase();
 
-            if (answer.equals("True") || answer.equals("False)) {
-                return answer.substring(0, 1);
+            if (isValidAnswer(answer)) {
+                return convertAnswer(answer);
             } else {
-                JOptionPane.showMessageDialog(null, "Invalid answer. Please enter True/False.");
+                JOptionPane.showMessageDialog(null, "Invalid answer. Please enter TRUE or FALSE.");
             }
+        }
+    }
+
+    private boolean isValidAnswer(String answer) {
+        return answer.equals("F") || answer.equals("FALSE") || answer.equals("N") || answer.equals("NO")
+                || answer.equals("T") || answer.equals("TRUE") || answer.equals("Y") || answer.equals("YES");
+    }
+
+    private String convertAnswer(String answer) {
+        if (answer.equals("F") || answer.equals("FALSE") || answer.equals("N") || answer.equals("NO")) {
+            return "FALSE";
+        } else {
+            return "TRUE";
         }
     }
 }
