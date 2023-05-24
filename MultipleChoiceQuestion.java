6a3b4ff1a7174f3ff00582d9abbb534a00f545e3
Gursavakhjhutty <117145051+Gursavakhjhutty@users.noreply.github.com>
2023-05-24 12:14:03 -0600

    Update MultipleChoiceQuestion.java

--- MultipleChoiceQuestion.java
+++ MultipleChoiceQuestion.java
@@ -1,31 +1,37 @@
 package src;
 
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
 
 public class MultipleChoiceQuestion extends Question {
-    public MultipleChoiceQuestion(String query, String a, String b, String c, String d, String e, String answer) {
+    MultipleChoiceQuestion(String query, String a, String b, String c, String d, String e, String answer) {
         super(query);
-        this.correctAnswer = answer;
-
         addChoice("A", a);
         addChoice("B", b);
         addChoice("C", c);
         addChoice("D", d);
         addChoice("E", e);
-
         initQuestionDialog();
+        correctAnswer = answer;
     }
 
     void addChoice(String name, String label) {
         JPanel choice = new JPanel(new BorderLayout());
-
         JButton button = new JButton(name);
         button.addActionListener(question);
-
         choice.add(button, BorderLayout.WEST);
         choice.add(new JLabel(label + " ", JLabel.LEFT), BorderLayout.CENTER);
-
         question.add(choice);
+    }
+
+    void check() {
+        String answer = ask();
+        nQuestions++;
+        if (answer.equals(correctAnswer)) {
+            JOptionPane.showMessageDialog(null,"Correct!");
+            nCorrect++;
+        } else {
+            JOptionPane.showMessageDialog(null,"Incorrect. The correct answer is " + correctAnswer + ".");
+        }
     }
 }
