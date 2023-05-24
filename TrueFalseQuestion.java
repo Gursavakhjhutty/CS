9f8108860700207af4b1c78d9866ceb73476060b
Gursavakhjhutty <117145051+Gursavakhjhutty@users.noreply.github.com>
2023-05-24 12:07:10 -0600

    Update TrueFalseQuestion.java

--- TrueFalseQuestion.java
+++ TrueFalseQuestion.java
@@ -1,27 +1,23 @@
 package src;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class TrueFalseQuestion extends Question {
     public TrueFalseQuestion(String question, String answer) {
-        this.question = "TRUE or FALSE: " + question;
-        this.correctAnswer = answer.equals("true") || answer.equals("yes") ? "TRUE" : "FALSE";
+        super(question);
+        this.correctAnswer = answer.equalsIgnoreCase("true") || answer.equalsIgnoreCase("yes") ? "TRUE" : "FALSE";
+
+        JPanel buttons = new JPanel();
+        addButton(buttons, "TRUE");
+        addButton(buttons, "FALSE");
+
+        this.question.add(buttons);
+        initQuestionDialog();
     }
 
-    public String ask() {
-        return ask(question);
-    }
-
-    private String ask(String question) {
-        while (true) {
-            String answer = JOptionPane.showInputDialog(question);
-            answer = answer.toUpperCase();
-
-            if (answer.equals("TRUE") || answer.equals("FALSE")) {
-                return answer;
-            } else {
-                JOptionPane.showMessageDialog(null, "Invalid answer. Please enter TRUE or FALSE.");
-            }
-        }
+    void addButton(JPanel buttons, String label) {
+        JButton button = new JButton(label);
+        button.addActionListener(question);
+        buttons.add(button);
     }
 }
